eda11b18ad1026a655747924264548e215e58489
Jahidul Islam Jihad <islamjahiduljihad@gmail.com>
2025-06-25 11:00:18 +0600

    Mongodb local connection added.

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 # Flask, Jsonify & Request import
 from flask import Flask, jsonify, request
+# MongoClient import
+from pymongo import MongoClient
 
 # Create app
 app = Flask(__name__)
+
+#Local Mongodb Connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client.libraryDB
 
 # Root Route
 @app.route('/')
